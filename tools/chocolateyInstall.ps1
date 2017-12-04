d48a7c896988496fcabdc548e35953cd4a11da6a
Paulo V C Oliveira <paulovt7@gmail.com>
2017-12-04 17:00:08 -0200

    Update to protoc 3.5.0 - fix checksumType to sha256

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 Install-ChocolateyZipPackage 'protoc' `
  'https://github.com/google/protobuf/releases/download/v3.5.0/protoc-3.5.0-win32.zip' `
  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
- -checksum 'DFEFF4309F119A1610EE902AA4FE7D06FFB03646787C84AD274FCFE251BCDA45' -checksumType 'md5'
+ -checksum 'DFEFF4309F119A1610EE902AA4FE7D06FFB03646787C84AD274FCFE251BCDA45' -checksumType 'sha256'
