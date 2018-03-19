d6ba5a40958118cdccb23fa3be658e1a6dd44f0f
Jordi Garcia <jordi@improbable.io>
2018-03-19 16:23:18 +0000

    Update to v3.5.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 Install-ChocolateyZipPackage 'protoc' `
- 'https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-win32.zip' `
+ 'https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-win32.zip' `
  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" `
- -checksum 'E46B3B7C5C99361BBDD1BBDA93C67E5CBF2873B7098482D85FF8E587FF596B23' -checksumType 'sha256'
+ -checksum 'B2AFF99DCAD1E2E7678FB8E4F2BFC8E8D30AC19D11896C16DC9A1339D6578800' -checksumType 'sha256'
